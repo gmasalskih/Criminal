8ac5a48f8f43157deee9f57d00e4ff2a55d70786
gmasalskih <gmasalskih@mail.ru>
2018-05-07 23:02:03 +0300

    Initial commit

---
+++ app/src/main/java/ru/cursorka/criminal/CrimeActivity.kt
@@ -0,0 +1,12 @@
+package ru.cursorka.criminal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CrimeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crime)
+    }
+}
